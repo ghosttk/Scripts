15e2f34b1e8618084016ff0b30cc1b47f26f9891
ghosttk <ghosttk@hotmail.com>
2019-06-19 23:31:23 +0800

    parse first chapter

--- cSpider.py
+++ cSpider.py
@@ -1,6 +1,7 @@
+#-*- coding:utf-8 -*-
 import argparse
-import lxml
 import requests
+from lxml import etree
 
 class Spider:
     def parseArgs(self):
@@ -8,16 +9,43 @@ class Spider:
         parser.add_argument('-u', '--url', help='base url')
         parser.add_argument('-s', '--start', help='start endpoint')
         parser.add_argument('-e', '--end', help='end endpoint')
+        parser.add_argument('-i', '--id', help='book id')
         args = parser.parse_args()
         self.baseUrl = args.url
         self.startPoint = args.start
         self.endPoint = args.end
+        self.bookId = args.id
+    def scrapById(self):
+        self.bookUrl = self.baseUrl + '/' + self.bookId + '/'
+        page=requests.get(self.bookUrl)
+        html=page.text
+        selector=etree.HTML(html)
+        content=selector.xpath('//a[@class="a1"]/@href')
+        if content:
+            self.startUrl = self.baseUrl + content[0]
+            return self.startUrl
+    def nextChapter(self, url):
+        page=requests.get(url)
+        html=page.text
+        selector=etree.HTML(html)
+        content = selector.xpath('//div[@id="chaptercontent"]/p/text()')
+        print(dir(content))
+        if content:
+            print(content)
+        '''
+        f = open(self.bookId + '.txt', 'a')
+        f.write(content[0])
+        f.close()
+        '''
+        nextUrl =selector.xpath('//a[@class="a4"]/@href')
+        if nextUrl:
+            self.nextUrl = self.baseUrl + nextUrl[0] +'.html'
+            return self.nextUrl
     def __init__(self):
         self.parseArgs()
 
 if __name__ == '__main__':
     spider = Spider()
-    if spider.endPoint :
-        print(spider.endPoint)
-
-
+    if spider.bookId:
+        url = spider.scrapById()
+        spider.nextChapter(url)
