3ee2489e12824e0ab261deb33b946598ec52af47
ghosttk <ghosttk@hotmail.com>
2019-06-27 17:15:15 +0800

    fix gbk decode

--- xiaohua.py
+++ xiaohua.py
@@ -1,25 +1,39 @@
 # -*- coding: utf-8 -*-  
 import urllib.request
-import re
 import sys
-import xml.etree.ElementTree as ET
+from html.parser import HTMLParser
 
-def scrap(baseurl='http://xiaohua.zol.com.cn/detail5/', iStart=1, iEnd=1):
+class MyParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.in_div = False
+    def _attr(self, attrlist, attrname):
+        for attr in attrlist:
+            if attr[0] == attrname:
+                return attr[1]
+        return None
+    def handle_starttag(self, tag, attrs):
+        if tag == 'div' and self._attr(attrs, 'class') == 'article-text':
+            self.in_div = True
+    def handle_data(self, data):
+        if self.in_div == True :
+            print("Encountered data  :")
+            with open('test.txt', 'w', encoding='utf-8') as wf:
+                wf.write(str(data))
+            #self.in_div = False 
+
+def scrap(baseurl='http://xiaohua.zol.com.cn/detail5/', iStart=59391, iEnd=59392):
     url = baseurl+str(iStart)+'.html'
-    f = urllib.request.urlopen(url)
+    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}  
+    req = urllib.request.Request(url, headers=headers)
+    f = urllib.request.urlopen(req)
     uf = f.read().decode('gbk')
-    with open('test.txt', 'w', encoding='utf-8') as opf:
-        opf.write(str(uf))
-    '''
-    uf = re.sub(u"[\x00-\x08\x0b-\x0c\x0e-\x1f]+",u"", uf)
-
-    root = ET.parse('test.txt')
-    article = root.find('//div[@class="article-text"]')
-    print(article)
-    with open(str(iEnd)+'.txt', 'a', encoding='gbk') as of:
-        of.write(uf)
-        '''
-    print(str(uf))
+    parser = MyParser()
+    #parser.feed(str(uf))
+    parser.feed(uf)
 
 if __name__ == '__main__':
-    scrap(sys.argv[1], sys.argv[2])
+    if len(sys.argv) == 3:
+        scrap(sys.argv[1] or None, sys.argv[2] or None)
+    else:
+        scrap()
